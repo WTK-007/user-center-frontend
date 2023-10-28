e64895056bf9da9f3796ce621ccccb8ebe059cd2
WTK-007 <99063969+WTK-007@users.noreply.github.com>
2023-10-28 21:21:29 +0800

    修改logo最终版

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const SYSTEM_LOGO = "jetbrains://web-storm/navigate/reference?project=user-center-frontend&path=src/pages/user/Login/program_logo.png";
+export const SYSTEM_LOGO = "https://gd-hbimg.huaban.com/ee8ca7e3996bd72f8f86bb19b85ce905ad63136a2bc3-2DmVzx_fw240webp";
 
 /**
  * 知识星球“知醒社”链接
