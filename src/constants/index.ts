e9c2c297ffea14e3102111afeb416693736b8863
WTK-007 <99063969+WTK-007@users.noreply.github.com>
2023-10-28 21:19:07 +0800

    修复常量文件错误

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-export const SYSTEM_LOGO = "https://gd-hbimg.huaban.com/ee8ca7e3996bd72f8f86bb19b85ce905ad63136a2bc3-2DmVzx_fw240webp";
-=======
-export const SYSTEM_LOGO = "https://gd-hbimg.huaban.com/fa9704111c128cf9b1ecf2f3e9a18e23f714ba18bf9c-WyFaMH_fw240webp";
->>>>>>> parent of b8a11c5 (修改系统主页的logo)
+export const SYSTEM_LOGO = "jetbrains://web-storm/navigate/reference?project=user-center-frontend&path=src/pages/user/Login/program_logo.png";
 
 /**
  * 知识星球“知醒社”链接
