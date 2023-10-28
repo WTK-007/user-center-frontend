328ad7651d0b94a9f1d809980b56e9e0e8c2eb6c
WTK-007 <99063969+WTK-007@users.noreply.github.com>
2023-10-28 21:16:07 +0800

    再次修改logo

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
+export const SYSTEM_LOGO = "https://gd-hbimg.huaban.com/ee8ca7e3996bd72f8f86bb19b85ce905ad63136a2bc3-2DmVzx_fw240webp";
+=======
 export const SYSTEM_LOGO = "https://gd-hbimg.huaban.com/fa9704111c128cf9b1ecf2f3e9a18e23f714ba18bf9c-WyFaMH_fw240webp";
+>>>>>>> parent of b8a11c5 (修改系统主页的logo)
 
 /**
  * 知识星球“知醒社”链接
